85ef8c52d240cf13a42e9f0b310b11d3c60d9b28
rafaelabarbieric <barbieridacruzrafaela@gmail.com>
2024-02-23 15:05:54 -0300

    criando endereço surge

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,23 @@ const contador = ref(0)
 </script>
 
 <template>
+
   <button @click="contador++">+</button>
    <button @click="contador--">-</button>
    <p> Valor do contador: {{ contador }}</p>
 
 </template>
-<style scoped> </style>
+
+<style scoped> 
+
+button{
+  background-color: rgb(104, 104, 250);
+  color: white;
+  font-weight: bold;
+  border: 0;
+  margin: 2px;
+  padding: 20px;
+}
+
+</style>
 
